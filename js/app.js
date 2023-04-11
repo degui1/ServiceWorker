cdec20acbb2b23b899edfed4686a8bc063e855c4
ggoncalves <gui.denez56@gmail.com>
2023-04-11 16:00:14 -0300

    using 'self'

--- js/app.js
+++ js/app.js
@@ -1,47 +1,44 @@
 const APP = {
-    sw: null,
+    SW: null,
     init() {
-        if ('serviceWorker' in navigator) {
-            // Delay registration until after the page has loaded, to ensure that our
-            // precaching requests don't degrade the first visit experience.
-            // window.addEventListener('load', function() {
-                // Your service-worker.js *must* be located at the top-level directory relative to your site.
-                // It won't be able to control pages unless it's located at the same level or higher than them.
-                // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-                navigator.serviceWorker.register('sw.js').then(function(registration) {
-                // updatefound is fired if service-worker.js changes.
-                    registration.onupdatefound = function() {
-                        // The updatefound event implies that reg.installing is set
-                        var installingWorker = registration.installing;
-                
-                        installingWorker.onstatechange = function() {
-                            switch (installingWorker.state) {
-                                case 'installed':
-                                    if (navigator.serviceWorker.controller) {
-                                        // At this point, the old content will have been purged and the fresh content will
-                                        // have been added to the cache.
-                                        // It's the perfect time to display a "New content is available; please refresh."
-                                        // message in the page's interface.
-                                        console.log('New or updated content is available.');
-                                    } else {
-                                        // At this point, everything has been precached.
-                                        // It's the perfect time to display a "Content is cached for offline use." message.
-                                        console.log('Content is now available offline!');
-                                    }
-                                    break;
-                    
-                                case 'redundant':
-                                    console.error('The installing service worker became redundant.');
-                                    break;
-                        }
-                        };
-                    };
-                }).catch(function(e) {
-                console.error('Error during service worker registration:', e);
-                });
-            // });
+      //called after DOMContentLoaded
+      if ('serviceWorker' in navigator) {
+        // 1. Register a service worker hosted at the root of the
+        // site using the default scope.
+        navigator.serviceWorker
+          .register('/sw.js', {
+            scope: '/',
+          })
+          .then((registration) => {
+            APP.SW =
+              registration.installing ||
+              registration.waiting ||
+              registration.active;
+            console.log('service worker registered');
+          });
+        // 2. See if the page is currently has a service worker.
+        if (navigator.serviceWorker.controller) {
+          console.log('we have a service worker installed');
         }
+  
+        // 3. Register a handler to detect when a new or
+        // updated service worker is installed & activate.
+        navigator.serviceWorker.oncontrollerchange = (ev) => {
+          console.log('New service worker activated');
+        };
+  
+        // 4. remove/unregister service workers
+        // navigator.serviceWorker.getRegistrations().then((regs) => {
+        //   for (let reg of regs) {
+        //     reg.unregister().then((isUnreg) => console.log(isUnreg));
+        //   }
+        // });
+        // 5. Listen for messages from the service worker
+      } else {
+        console.log('Service workers are not supported.');
+      }
     },
-};
-
-document.addEventListener('DOMContentLoaded', APP.init);
+  };
+  
+  document.addEventListener('DOMContentLoaded', APP.init);
+  
--- sw.js
+++ sw.js
@@ -1,2 +1,32 @@
-// serviceWorker
-console.log('This is our service worker')
+// There's a variable called 'self' that represents serviceWorker itself
+// Take a look:
+// console.log({ self });
+
+// Self has access to most of the things that would in the window object
+// You do not have access to the DOM
+// Self can access the server
+// send fetch calls from service worker to get files (because you can not access the DOM)
+// and can storage files in the cache storage almost as a proxy server
+
+//some events that you have in the service worker
+
+self.addEventListener('install', (event) => {
+// When service worker is installed
+    console.log('Installed');
+});
+
+self.addEventListener('activate', (event) => {
+// when service worker is activated
+    console.log('Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+//used to intercept a fetch call
+// css file and app file are examples of fetch's that it will intercept
+    console.log('intercepted a http request', event.request);
+});
+
+self.addEventListener('message', (event) => {
+// message form webpage
+
+});

c9a314e1ba706e30c3d582aee5804210e49efbf4
ggoncalves <gui.denez56@gmail.com>
2023-05-19 16:54:40 -0300

    .estimate .persist | discovering cache size using cache

--- js/app.js
+++ js/app.js
@@ -2,7 +2,39 @@ const APP = {
     SW: null,
     init() {
         APP.registerSW();
+        APP.getCacheSize();
         document.querySelector('header>h2').addEventListener('click', APP.addImage);
+    },
+    getCacheSize() {
+        if ('storage' in navigator) {
+            if ('estimate' in navigator.storage) {
+                // get the total storage and current usage
+                navigator.storage.estimate().then(({usage, quota, usageDetails}) => {
+                    //returned numbers are in bytes
+                    let usageKB = parseInt(usage / 1024);
+                    let quotaKB   = parseInt(quota / 1024);             
+                    console.log(`Using ${usageKB} KB of ${quotaKB} KB`);
+                })
+                // see if the storage can be set to persistent or stay best-effort
+                navigator.storage.persist().then(isPer => {
+                    console.log(`Browser grants persistent permission: ${isPer}`);
+                })
+            }
+        } else console.warn('Storage Api is not supported');
+
+        // look at individual files and their sizes
+        caches.open('imageCache-5').then(cache => {
+            cache.matchAll().then(matches => {
+                let total = 0;
+                matches.forEach(resp => {
+                    if (resp.headers.has('content-length')) {
+                        total += parseInt(resp.headers.get('content-length'));
+                        console.log(`Adding size for ${resp.url}`);
+                    }
+                });
+                console.log(`Total size in imageCache-1 is ${total}`);
+            })
+        })
     },
     registerSW() {
         if ('serviceWorker' in navigator) {

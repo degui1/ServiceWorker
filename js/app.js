b4a40c8204322b1da392c7e2b7359fec0779b05e
ggoncalves <gui.denez56@gmail.com>
2023-05-23 15:21:32 -0300

    Controlling every fetch call

--- js/app.js
+++ js/app.js
@@ -1,68 +1,47 @@
 const APP = {
     SW: null,
     init() {
-        APP.registerSW();
-        APP.getCacheSize();
-        document.querySelector('header>h2').addEventListener('click', APP.addImage);
-    },
-    getCacheSize() {
-        if ('storage' in navigator) {
-            if ('estimate' in navigator.storage) {
-                // get the total storage and current usage
-                navigator.storage.estimate().then(({usage, quota, usageDetails}) => {
-                    //returned numbers are in bytes
-                    let usageKB = parseInt(usage / 1024);
-                    let quotaKB   = parseInt(quota / 1024);             
-                    console.log(`Using ${usageKB} KB of ${quotaKB} KB`);
-                })
-                // see if the storage can be set to persistent or stay best-effort
-                navigator.storage.persist().then(isPer => {
-                    console.log(`Browser grants persistent permission: ${isPer}`);
-                })
-            }
-        } else console.warn('Storage Api is not supported');
-
-        // look at individual files and their sizes
-        caches.open('imageCache-5').then(cache => {
-            cache.matchAll().then(matches => {
-                let total = 0;
-                matches.forEach(resp => {
-                    if (resp.headers.has('content-length')) {
-                        total += parseInt(resp.headers.get('content-length'));
-                        console.log(`Adding size for ${resp.url}`);
-                    }
-                });
-                console.log(`Total size in imageCache-1 is ${total}`);
-            })
-        })
+      //called after DOMContentLoaded
+      //register our service worker
+      APP.registerSW();
+      document.querySelector('h2').addEventListener('click', APP.addImage);
     },
     registerSW() {
-        if ('serviceWorker' in navigator) {
-
-            navigator.serviceWorker.register('./sw.js').then(
-                (reg => {
-                    APP.SW = 
-                        reg.installing ||
-                        reg.waiting ||
-                        reg.active;
-                    
-                }),
-                (error => {
-                    console.log('Service Worker registration failed', error);
-                })
-            )
-        } else {
-            console.log('Service workers are not supported');
-        }
+      if ('serviceWorker' in navigator) {
+        // Register a service worker hosted at the root of the site
+        navigator.serviceWorker.register('/sw.js').then(
+          (registration) => {
+            APP.SW =
+              registration.installing ||
+              registration.waiting ||
+              registration.active;
+          },
+          (error) => {
+            console.log('Service worker registration failed:', error);
+          }
+        );
+      } else {
+        console.log('Service workers are not supported.');
+      }
     },
     addImage(ev) {
-        let img = document.createElement('img'); 
-        img.src = '/img/x.jpg?id=one';
-        img.alt = 'image';
-        let p = document.createElement('p');
+      let img = document.createElement('img');
+      let p = document.createElement('p');
+      let main = document.querySelector('main');
+      //handle the load and error events for the image
+      img.addEventListener('load', (ev) => {
         p.append(img);
-        document.querySelector('main').append(p);
+        main.insertBefore(p, main.querySelector('p'));
+      });
+      img.addEventListener('error', (err) => {
+        //don't bother adding a broken image
+        p.textContent = 'Sorry. Your new image cannot be found';
+        main.insertBefore(p, main.querySelector('p'));
+      });
+      //try to load the image
+      img.src = '/img/x.jpg';
+      img.alt = 'dynamically added image';
     },
-}
-
-document.addEventListener('DOMContentLoaded', APP.init);
+  };
+  
+  document.addEventListener('DOMContentLoaded', APP.init);
--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const version = 5;
+const version = 2;
 let staticName = `staticCache-${version}`;
 let dynamicCache = 'dynamicCache';
-let fontName = `fontCache-${version}`;
 let imgName = `imageCache-${version}`;
 
-let assets = ['/','/index.html','/js/app.js'];
+let assets = ['/', '/index.html', '/js/app.js', '/css/main.css', '404.html'];
 
 let imgAssets = [
     '/img/j.jpg',
@@ -15,6 +14,7 @@ let imgAssets = [
     '/img/x.jpg?id=two',
     '/img/j.jpg?id=three',
     '/img/x.jpg?id=three',
+    '/img/notFound.jpg'
 ];
 
 
@@ -24,25 +24,25 @@ self.addEventListener('install', e => {
     // self.skipWaiting(); // skip the wainting to activate
     //but the page will not use the new sw yet
     caches.open(staticName).then(cache => {
-        cache.addAll(assets).then(()=> {
+        cache.addAll(assets).then(() => {
             console.log(`${staticName} has been updated`);
         },
-        (error => {
-            console.warn(`failed to update ${staticName}`);
-        })
+            (error => {
+                console.warn(`failed to update ${staticName}`);
+            })
         );
     })
-    .then(() => {
-        caches.open(imgName).then(cache => {
-            cache.addAll(imgAssets).then(() => {
-                console.log(`${imgName} has been updated`);
-            },
-            (error => {
-                console.warn(`failed to update ${imgName}`);
-            })
-            );
-        });
-    })
+        .then(() => {
+            caches.open(imgName).then(cache => {
+                cache.addAll(imgAssets).then(() => {
+                    console.log(`${imgName} has been updated`);
+                },
+                    (error => {
+                        console.warn(`failed to update ${imgName}`);
+                    })
+                );
+            });
+        })
 
 });
 
@@ -55,7 +55,7 @@ self.addEventListener('activate', e => {
                 keys.filter(key => {
                     if (key != staticName && key != imgName) return true;
                 })
-                .map(key => caches.delete(key))
+                    .map(key => caches.delete(key))
             ).then(empty => {
 
             });
@@ -63,10 +63,84 @@ self.addEventListener('activate', e => {
     )
 });
 
-self.addEventListener('fetch', e => {
-    console.log('intercepted a http request', e.request);
-});
+self.addEventListener('fetch', (e) => {
+    // Extendable Event.
+    console.log(`MODE: ${e.request.mode} for ${e.request.url}`);
+  
+    e.respondWith(
+      caches.match(e.request).then((cacheRes) => {
+        return (
+          cacheRes ||
+          Promise.resolve().then(() => {
+            let opts = {
+              mode: e.request.mode, //cors, no-cors, same-origin, navigate
+              cache: 'no-cache',
+            };
+            if (!e.request.url.startsWith(location.origin)) {
+              //not on the same domain as my html file
+              opts.mode = 'cors';
+              opts.credentials = 'omit';
+            }
+            return fetch(e.request.url, opts).then(
+              (fetchResponse) => {
+                //we got a response from the server.
+                if (fetchResponse.ok) {
+                  return handleFetchResponse(fetchResponse, e.request);
+                }
+                //not ok 404 error
+                if (fetchResponse.status == 404) {
+                  if (e.request.url.match(/\.html/i)) {
+                    return caches.open(staticName).then((cache) => {
+                      return cache.match('/404.html');
+                    });
+                  }
+                  if (
+                    e.request.url.match(/\.jpg$/i) ||
+                    e.request.url.match(/\.png$/i)
+                  ) {
+                    return caches.open(imageName).then((cache) => {
+                      return cache.match('/img/distracted-boyfriend.jpg');
+                    });
+                  }
+                }
+              },
+              (err) => {
+                //this is the network failure
+                //return the 404.html file if it is a request for an html file
+                if (e.request.url.match(/\.html/i)) {
+                  return caches.open(staticName).then((cache) => {
+                    return cache.match('/404.html');
+                  });
+                }
+              }
+            );
+            //.catch()
+          })
+        );
+      }) //end of match().then()
+    ); //end of respondWith
+  }); //end of fetch listener
+  
+  const handleFetchResponse = (fetchResponse, request) => {
+    let type = fetchResponse.headers.get('content-type');
+    // console.log('handle request for', type, request.url);
+    if (type && type.match(/^image\//i)) {
+      //save the image in image cache
+      console.log(`SAVE ${request.url} in image cache`);
+      return caches.open(imageName).then((cache) => {
+        cache.put(request, fetchResponse.clone());
+        return fetchResponse;
+      });
+    } else {
+      //save in dynamic cache - html, css, fonts, js, etc
+      console.log(`SAVE ${request.url} in dynamic cache`);
+      return caches.open(dynamicName).then((cache) => {
+        cache.put(request, fetchResponse.clone());
+        return fetchResponse;
+      });
+    }
+  };
 
 self.addEventListener('message', e => {
- //messa from webpage
+    //messa from webpage
 });
